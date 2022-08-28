2856586b832a02c933c0a23cfed51605ba00e01d
Tomasz Belech <tomaszbelech1@gmail.com>
2022-08-28 15:45:25 +0200

    app enviroment installed

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const data = {
+        name: 'chudy',
+        isAwesome: true
+    };
+
+    res.json(data);
+});
+
+app.get('/awesome-generator' , (req, res) => {
+    const { name, isAwesome} = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
 });
 
 const port = 8888;
