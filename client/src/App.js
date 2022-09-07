b7c28282a6257928f80d09996a0224fef574abae
Tomasz Belech <tomaszbelech1@gmail.com>
2022-09-07 13:17:19 +0200

    created spotify.js file and added access token to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import {accessToken} from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(()=> {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
-    }
-
+    setToken(accessToken);
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {!token ? (
+          <a
           className="App-link"
           href="http://localhost:8888/login">
           Log in to spotify
         </a>
+        ) : (
+          <h1>Logged in!</h1>
+        )}
       </header>
     </div>
   );
