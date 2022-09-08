9a7884e18384839460e3752008e1c89256248fc3
Tomasz Belech <tomaszbelech1@gmail.com>
2022-09-08 13:26:24 +0200

    added profile name,followers count and profile image

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
 import {useState, useEffect} from 'react';
-import {accessToken, logout} from './spotify';
+import {accessToken, logout, getCurrentUserProfile} from './spotify';
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+  
   useEffect(()=> {
     setToken(accessToken);
+
+    const fetchData = async () => {
+        try{
+            const {data} = await getCurrentUserProfile();
+            setProfile(data);
+            console.log(data);
+        }catch(e) {
+            console.error(e);
+
+        }
+    }
+
+    fetchData();
+
   }, []);
 
   return (
@@ -22,6 +38,17 @@ function App() {
           <>
             <h1>Logged in!</h1>
             <button onClick={logout}>Log Out</button>
+
+              {profile && (
+                  <>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                          <img src ={profile.images[0].url} alt="Avatar" />
+                      )}
+                  </>
+
+              )}
           </>
           
         )}
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -118,3 +118,14 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+axios.defaults.baseURL = `https:api.spotify.com/v1`;
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = `application/json`;
+
+/**
+ * get current user profile
+ * @returns {Promise}
+ */
+
+export const getCurrentUserProfile = () => axios.get('/me');
